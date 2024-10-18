2faafa4e04c3f606d8e23582aa1fb52c749eba32
Senthil Nathan R <131766412+Senthil-Nathan-R@users.noreply.github.com>
2024-10-19 00:22:03 +0530

    Update Connect.jsx

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -10,7 +10,7 @@ function Connect() {
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute left-[10%] top-[20%] animate-rotate-up-down-left' />
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute right-[10%] top-[30%] rotate-[78deg] animate-rotate-up-down-right' />
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute left-2/4 bottom-[5%] rotate-[-90deg] animate-rotate-left-right' />
-            <p className='text-white relative text-4xl md:text-[44px] text-center font-bold font-cairo mx-3 tracking-wide'>Let's create something <i>amazing</i> together!</p>
+            <p className='text-white relative text-4xl md:text-[44px] sm:text-[30px] text-center font-bold font-cairo mx-3 tracking-wide'>Let's create something <i>amazing</i> together!</p>
             <img src={arrow} alt="arrow-img" className='translate-x-10 mt-4 md:mt-7 mb-2  ' />
             <CustomButton
                 text="Send a message"
